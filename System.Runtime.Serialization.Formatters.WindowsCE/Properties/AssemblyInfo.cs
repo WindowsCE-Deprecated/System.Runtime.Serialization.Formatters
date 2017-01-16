d8a908d641c34de5db84f470571258fb4a165432
Fabrício Godoy <fabricio@verion.ind.br>
2017-01-16 11:27:42 -0200

    Improved building ans small fixes
    - New key for assembly signing.
    - Added LICENSE file.
    - Fix encoding for Windows CE files.
    - Improved assembly information and NuGet specification.

--- System.Runtime.Serialization.Formatters.WindowsCE/Properties/AssemblyInfo.cs
+++ System.Runtime.Serialization.Formatters.WindowsCE/Properties/AssemblyInfo.cs
@@ -1,16 +1,26 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+using System.Resources;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: NeutralResourcesLanguage("en-us")]
+[assembly: AssemblyCompany("Fabrício Godoy")]
+[assembly: AssemblyCopyright("© Fabrício Godoy. All rights reserved.")]
 [assembly: AssemblyProduct("System.Runtime.Serialization.Formatters")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyDescription("Provides common types for libraries that support runtime serialization")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.1")]
+[assembly: AssemblyFileVersion("1.0.1")]
+
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Retail")]
+#endif
 
 #if CLASSIC
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0")]
+[assembly: AssemblyKeyFile(@"..\..\..\tools\keypair.snk")]
+[assembly: AssemblyDelaySign(true)]
 #endif
